c12c34969fe0771598e55ad05b777639e0475120
Morten Falk <mfjo@itu.dk>
2023-09-11 16:39:39 +0200

    Upgrade not working yet

--- src/components/UpgradeButton.js
+++ src/components/UpgradeButton.js
@@ -1,31 +1,29 @@
-import React, {useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
-const UpgradeButton = ({ text, onClick, cost, onUpgrade, resources, storageKey }) => {
+const UpgradeButton = ({ text, cost, storageKey }) => {
+  const [count, setCount] = useState(() => {
+    const savedCount = parseInt(localStorage.getItem(storageKey)) || 0;
+    return savedCount;
+  });
 
-    var count = parseInt(localStorage.getItem(storageKey)) || 0;
-    var canUpgrade = count > parseInt(cost);
-    const handleUpgrade = () => {
-        if (canUpgrade) {
-          count = count - parseInt(cost);
-        } else {
-          alert("Cannot upgrade now. Check your resources or woodCount.");
-        }
-      };
-    
-      useEffect(() => {
-        // Update the canUpgrade variable when woodCount changes
-        const woodCountFromStorage =
-          parseInt(localStorage.getItem("woodCount")) || 0;
-        if (woodCountFromStorage > 50 && cost <= woodCountFromStorage) {
-          canUpgrade = true;
-        } else {
-          canUpgrade = false;
-        }
-      }, [cost]);
+  const handleButtonClick = () => {
+    if (count >= cost) {
+      const newCount = count - cost;
+      setCount(newCount);
+      localStorage.setItem(storageKey, newCount.toString());
+    } else {
+      alert("Not enough resources!");
+    }
+  };
+
+  useEffect(() => {
+    // No need to re-fetch the savedCount here, useState initializer handles it
+  }, []);
+
   return (
     <button
       className="w-1/2 border-gray-500 border-2 rounded-xl p-4 text-center transition duration-500 hover:bg-gray-300"
-      onClick={handleUpgrade}
+      onClick={handleButtonClick}
     >
       <div className="flex-col justify-center">
         {text}
