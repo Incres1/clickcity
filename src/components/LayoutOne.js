83e6a7aad339fed2006f994c1fdf736cc04e39bb
Morten Falk <mfjo@itu.dk>
2023-09-11 15:58:27 +0200

    Division of LayoutOne

--- src/components/LayoutOne.js
+++ src/components/LayoutOne.js
@@ -2,14 +2,18 @@ import React from "react";
 import ClickButton from "./ClickButton";
 
 const LayoutOne = ({ children }) => {
-    return (
-        <div className="space-x-4 flex">
-            <ClickButton text="Wood" storageKey="woodCount" />
-            <ClickButton text="Metal" storageKey="metalCount" />
-        
+  return (
+    <div className="flex flex-col h-screen">
+      <div className="flex h-1/2 space-x-4">
+        <ClickButton text="Wood" storageKey="woodCount" />
+        <ClickButton text="Metal" storageKey="metalCount" />
+      </div>
 
-        </div>
-    );
-    };
+      <div className="flex h-1/2 justify-center items-center text-black">
+        This is the second half.
+      </div>
+    </div>
+  );
+};
 
 export default LayoutOne;
