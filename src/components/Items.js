64f65ef8815346131a6d91424f70df7f45e24114
Morten Falk <mfjo@itu.dk>
2023-09-13 10:28:52 +0200

    refactor a bit

--- src/components/Items.js
+++ src/components/Items.js
@@ -2,139 +2,128 @@ import React, { useState, useEffect } from "react";
 import Materials from "./Materials";
 
 const Items = () => {
-  const { woodCount, oreCount, leaf, gem, updateWoodCount,
-    updateOreCount,updateLeafCount,
-    updateGemCount, } = Materials();
+  const { woodCount, oreCount, leaf, gem, updateWoodCount, updateOreCount, updateLeafCount, updateGemCount } = Materials();
 
 
-  const initialSimpleSword = {
-    name: "Simple Sword",
-    strength: 2,
-    wood: 200,
-    ore: 200,
-    leaf: 0,
-    gem: 1,
-    count: 0,
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  // Define items as an object with keys as item names
+  const itemList = {
+    simpleSword: {
+      name: "Simple Sword",
+      strength: 2,
+      wood: 200,
+      ore: 200,
+      leaf: 0,
+      gem: 1,
+      count: 0,
+    },
+    simpleShield: {
+      name: "Simple Shield",
+      health: 2,
+      wood: 200,
+      ore: 200,
+      leaf: 1,
+      gem: 0,
+      count: 0,
+    },
   };
 
-  const initialSimpleShield = {
-    name: "Simple Shield",
-    health: 2,
-    wood: 200,
-    ore: 200,
-    leaf: 1,
-    gem: 0,
-    count: 0,
-  };
 
-  const itemList = [initialSimpleShield, initialSimpleSword];
-
-  const [selectedItem, setSelectedItem] = useState(null); // Track selected item
-
-  const handleItemClick = (item) => {
-    setSelectedItem(item); // Set the selected item
-  };
-
+  // Retain state for simpleSword and simpleShield
   const [simpleSword, setSimpleSword] = useState(
-    JSON.parse(localStorage.getItem("simpleSword")) || initialSimpleSword
+    JSON.parse(localStorage.getItem("simpleSword")) || itemList.simpleSword
   );
   const [simpleShield, setSimpleShield] = useState(
-    JSON.parse(localStorage.getItem("simpleShield")) || initialSimpleShield
+    JSON.parse(localStorage.getItem("simpleShield")) || itemList.simpleShield
   );
-
-  useEffect(() => {
-    const storedSimpleSword =
-      JSON.parse(localStorage.getItem("simpleSword")) || initialSimpleSword;
-    const storedSimpleShield =
-      JSON.parse(localStorage.getItem("simpleShield")) || initialSimpleShield;
-
-    setSimpleSword(storedSimpleSword);
-    setSimpleShield(storedSimpleShield);
-  }, []);
 
   
 
+  // Define a function to handle crafting
+  const handleCraftClick = (itemName, wood) => {
+    const itemDetails = itemList[itemName]; // Access the details using the itemName
+    
+    if (
+      woodCount >= itemDetails.wood &&
+      oreCount >= itemDetails.ore &&
+      leaf >= itemDetails.leaf &&
+      gem >= itemDetails.gem
+    ) {
+      const newWoodCount = woodCount - itemDetails.wood;
+      const newOreCount = oreCount - itemDetails.ore;
+      const newLeafCount = leaf - itemDetails.leaf;
+      const newGemCount = gem - itemDetails.gem;
+      updateWoodCount(newWoodCount);
+      updateOreCount(newOreCount);
+      updateLeafCount(newLeafCount);
+      updateGemCount(newGemCount);
+      const message = `You have crafted a ${itemDetails.name}!`;
+      alert(message);
+    } else {
+      alert("You do not have enough resources to craft!");
+    }
+  };
 
-  const CraftButton = ({item}) => {
-
-    const handleCraftClick = () => {
-      if (woodCount >= item.wood && oreCount >= item.ore && leaf >= item.leaf && gem >= item.gem) {
-        const newWoodCount = woodCount - item.wood;
-        const newOreCount = oreCount - item.ore;
-        const newLeafCount = leaf - item.leaf;
-        const newGemCount = gem - item.gem;
-        updateWoodCount(newWoodCount);
-        updateOreCount(newOreCount);
-        updateLeafCount(newLeafCount);
-        updateGemCount(newGemCount);
-        const message = `You have crafted a ${item.name}!`;
-        alert(message);
-      } else {
-        alert("You do not have enough resources to craft!");
-      }
-    };
+  // Render the CraftButton component for each item
+  const CraftButton = ({ itemName}) => {
 
     return (
       <button
         className="w-1/4 h-1/4 border-gray-500 border-2 rounded-xl p-4 text-center transition duration-500 hover:bg-gray-300"
-        onClick={handleCraftClick}
+        onClick={() => handleCraftClick(itemName)}
       >
         <div className="flex-col justify-center">
-          Craft {item.name}
-          </div>
+          Craft {itemList[itemName].name}
+        </div>
       </button>
     );
   };
 
-
-
-
-
-
   useEffect(() => {
-    localStorage.setItem("simpleSword", JSON.stringify(simpleSword));
-    localStorage.setItem("simpleShield", JSON.stringify(simpleShield));
     localStorage.setItem("woodCount", woodCount);
     localStorage.setItem("oreCount", oreCount);
     localStorage.setItem("leaf", leaf);
     localStorage.setItem("gem", gem);
-  }, [simpleSword, simpleShield, woodCount, oreCount, leaf, gem]);
-
-    
+  }, [woodCount, oreCount, leaf, gem]);
 
   return (
     <div>
       <ul>
-        {itemList.map((item) => (
-          <li key={item.name}>
+        {Object.keys(itemList).map((itemName) => (
+          <li key={itemName}>
             <div
               className={`bg-white p-4 text-left ${
-                selectedItem === item ? "bg-black" : ""
+                selectedItem === itemName ? "bg-black" : ""
               }`}
-              onClick={() => handleItemClick(item)}
+              onClick={() => setSelectedItem(itemName)}
             >
-              <div className="text-2xl font-semibold">{item.name}</div>
-              <div className="text-gray-600">Strength: {item.strength || 0}</div>
-              <div className="text-gray-600">Health: {item.health || 0}</div>
-
-              <div className="text-gray-600">Count: {item.count}</div>
+              <div className="text-2xl font-semibold">
+                {itemList[itemName].name}
+              </div>
+              <div className="text-gray-600">
+                Strength: {itemList[itemName].strength || 0}
+              </div>
+              <div className="text-gray-600">
+                Health: {itemList[itemName].health || 0}
+              </div>
+              <div className="text-gray-600">
+                Count: {itemList[itemName].count}
+              </div>
             </div>
           </li>
         ))}
       </ul>
 
-      {/* Display selected item details */}
       {selectedItem && (
         <div className="p-4 text-left rounded-xl bg-amber-400">
           <div className="text-2xl font-semibold">
-            Do you want to craft {selectedItem.name}?
-            </div>
-            <div className="text-2xl font-semibold">
-            Cost: {selectedItem.wood || 0} wood, {selectedItem.ore || 0} ore, {selectedItem.gem || 0} gem, {selectedItem.leaf || 0} leaf
-            </div>
-            <CraftButton item={selectedItem}/>
-          {/* Display other details of the selected item */}
-          {/* Add buttons or actions for the selected item */}
+            Do you want to craft {itemList[selectedItem].name}?
+          </div>
+          <div className="text-2xl font-semibold">
+            Cost: {itemList[selectedItem].wood || 0} wood, {itemList[selectedItem].ore || 0} ore, {itemList[selectedItem].gem || 0} gem, {itemList[selectedItem].leaf || 0} leaf
+          </div>
+          <CraftButton itemName={selectedItem} />
         </div>
       )}
     </div>
