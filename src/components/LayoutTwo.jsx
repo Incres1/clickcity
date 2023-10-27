b50e339e2bbf34fe7660b6c7e0dc390d3b4be33a
Morten Falk <mfjo@itu.dk>
2023-10-27 16:39:53 +0200

    Update LayoutTwo.jsx

--- src/components/LayoutTwo.jsx
+++ src/components/LayoutTwo.jsx
@@ -90,7 +90,7 @@ const LayoutTwo = () => {
   //CARD FOR DISPLAYING OTHER CHARACTER DETAILS
   const LevelCard = ({ text, type }) => {
     return (
-      <div className="max-content">
+      <div className="max-content flex-wrap">
         <div className="bg-white p-4 rounded-lg shadow-md text-center">
           <div className="text-2xl font-semibold">{text}</div>
           <div className="text-gray-600">{type}</div>
